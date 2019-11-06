f1b3655e954352ff654802353d3e19c84361edd1
Julia Mitas <juliamitas.lote@gmail.com>
2019-11-06 18:43:21 +0100

    Created a project named "Polybius Square".

---
+++ Polybius square/Polybius square/polybiusSquare.cpp
@@ -0,0 +1,18 @@
+#include "polybiusSquare.h"
+
+std::string getKeyWord()
+{
+	std::string keyWord;
+	std::cout << "Write a key word with which your polybius square will be created:\n";
+	std::cin >> keyWord;
+	return keyWord;
+}
+
+bool checkValidityOfKeyWord(std::string keyWord)
+{
+	bool valid = false;
+
+
+
+	return valid;
+}
